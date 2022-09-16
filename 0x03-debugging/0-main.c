9604b5a9c257ca006d05dd3d991b0ff977981180
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-09-16 10:20:13 -0700

    Task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,8 +1,8 @@
-#inlude "main.h"
+#include "main.h"
 
 /**
- *  main - tests function that prints if integer is positive or negative
- *  Return: 0
+ * main - tests function that prints if integer is positive or negative*
+ * Return: 0 *
  */
 
 int main(void)
@@ -14,4 +14,3 @@ int main(void)
 
 	return (0);
 }
-
