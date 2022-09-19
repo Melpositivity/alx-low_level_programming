b3a636b63a1c609eb3284638f13904e7edd0745a
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-09-19 07:36:21 -0700

    Task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -8,16 +8,15 @@
  * Description: prints a string
  * On success: returns no error
  */
-
 void _puts(char *str)
 {
 	int i = 0;
 
-		while (*(str + i) != '\0')
-				{
-					putchar(*(str + i));
-					i++;
-				}
-					
-				putchar(10);
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	putchar(10);
 }
+
