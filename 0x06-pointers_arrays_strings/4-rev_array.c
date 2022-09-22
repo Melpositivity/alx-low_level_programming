41fd6bec0252a62caa513c47bfe77b1db6bcd9d9
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-09-22 11:33:18 -0700

    Task 4

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverse an integer array
+ * @a: string to reverse
+ * @n: the int length
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int index;
+	int copy;
+
+	n = n - 1;
+
+	for (index = 0; index < n; index++)
+	{
+		copy = *(a + index);
+		*(a + index) = *(a + n);
+		*(a + n) = copy;
+		n--;
+	}
+}
+
