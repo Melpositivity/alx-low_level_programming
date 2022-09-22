5ed7207c8d6f2806fd7f586f6316aa3d4504b484
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-09-22 12:19:29 -0700

    Task 0

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,33 +1,27 @@
 #include "main.h"
 
 /**
- * _strcat - print reversed strings
- * @dest: string to concatenate
- * @src: to contatenate string
- * Return: array pointer dest
+ * _strcat - concatenates  the string pointed to by @src to
+ * the end of the string pointed to by @dest
+ * @dest: String that will be appended
+ * @src: String to be concatenated upon
+ *
+ * Return: returns poiner to @dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int index;
-	int index2;
-	char strpiler[100]
+	int index = 0, dest_len = 0;
 
-		for (index = 0; *(dest + index) != '\0'; index++)
-		{
-			*(strpiler + index) = *(dest + index);
-		}
-	for (index2 = 0; *(src + index2) != '\0'; index2++)
-	{
-		*(strpiler + index) = *(src + index2);
-		index++;
-	}
-	*(strpiler + index) = *(src + index2);
-	for (index = 0; *(strpiler + index) != '\0'; index++)
-	{
-		*(dest + index) = *(strpiler + index);
-	}
-	*(dest + index) = *(strpiler + index);
+
+	while (dest[index++])
+		dest_len++;
+
+	for (index = 0; src[index]; index++)
+		dest[dest_len++] = src[index];
+
+
 	return (dest);
 }
+
 
