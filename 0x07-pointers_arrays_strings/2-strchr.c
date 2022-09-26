6eb5ad7f5d67e0da105d44fd21edaf3912dd9476
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-09-26 13:16:41 -0700

    Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *  _strchr - locates character in a string
+ *
+ *  @s: the byte to write
+ *  @c: the objective to locate
+ *  Return: Returns pointer
+ */
+
+char *_strchr(char *s, char c)
+{
+	int index;
+
+	for (index = 0; s[index] || s[index] == '\0'; index++)
+
+		{
+			if (s[index] == c)
+				return (&s[index]);
+			else if (s[index] == '\0')
+				break;
+		}
+	return (NULL);
+}
+
