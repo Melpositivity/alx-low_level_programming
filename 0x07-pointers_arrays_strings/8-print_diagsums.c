2a720ead7230440b5b2338aa1372dd9de0231425
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-09-27 08:04:20 -0700

    Task 8

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,14 +1,25 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * set_string - sets the value of a pointer to a char
- * @s: pointer to change
- * @to: string to change pointer to
- *
- * Return: void
+ * print_diagsums - sums diagonals of a square
+ * @a: the square of integers
+ * @size: the size of the square
+ * Return: Always 0.
  */
-void set_string(char **s, char *to)
+void print_diagsums(int *a, int size)
 {
-	*s = to;
+	int i, sum1, sum2;
+
+	sum1 = 0;
+	sum2 = 0;
+	for (i = 0; i < size * size; i += size + 1)
+		sum1 += a[i];
+	for (i = size - 1; i < size * size - 1; i += size - 1)
+		sum2 += a[i];
+	printf("%d, %d\n", sum1, sum2);
+
+
 }
+
 
