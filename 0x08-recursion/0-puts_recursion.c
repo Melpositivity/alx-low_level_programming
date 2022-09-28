96a2d0311e2387da97a79a0500926060ad52bdbe
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-09-28 06:11:52 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#ifndef MAIN_H
+#define MAIN-H
 
 /**
  * _puts_recursion - copies a string
@@ -10,6 +11,7 @@ void _puts_recursion(char *s)
 	if (*s != '\0')
 	{
 		_putchar(*s);
+
 		_puts_recursion(s + 1);
 	}
 	else
