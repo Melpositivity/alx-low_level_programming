6cb495cf7e2d4dd9c725f884236ea7f780206735
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-09-28 05:47:32 -0700

    C _ Recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -9,9 +9,14 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
+
 	{
-		_print_rev_recursion(s + 1);
-		_putchar(*(s));
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else
+	{
+		_putchar('\n');
 	}
 }
 
