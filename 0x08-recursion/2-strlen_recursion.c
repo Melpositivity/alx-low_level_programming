84f8cd3d1a6b65c1badee1b21d10f9c94b485371
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-09-29 07:30:35 -0700

    Task 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -6,20 +6,11 @@
  *
  * Return: The length of the string.
  */
-
 int _strlen_recursion(char *s)
 {
-	 int len = 0;
-
-	  if (*s)
-	  {
-		  len++; 
-		 len += _strlen_recursion(s + 1);
-	  }
-	 return (len); 
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
 }
-
-
-		 
- 
-
