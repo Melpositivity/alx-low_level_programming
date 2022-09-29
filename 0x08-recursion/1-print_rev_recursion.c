30845949ce6d2167a9295538eb02c0303c3af9a9
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-09-29 05:18:49 -0700

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+
+/**
+ * _print_rev_recursion - copies a string
+ * @s: String
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
+
