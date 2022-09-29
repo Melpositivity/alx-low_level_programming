1efd018c1a9f79be2affb890f622cdf66bb8bb76
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-09-29 08:11:30 -0700

    Task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -6,26 +6,19 @@
  * @n: INput
  * Return: value of stringlength
  */
-
 int factorial(int n)
-
 {
 	if (n < 0)
-
 	{
 		return (-1);
 	}
 	else if (n == 0)
-
 	{
 		return (1);
 	}
 	else
-
 	{
 		return (n * factorial(n - 1));
 	}
-
 }
 
- 
