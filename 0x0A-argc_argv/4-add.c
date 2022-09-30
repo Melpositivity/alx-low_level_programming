5bfc16c37d4d5fe39c02a94c7355bbe4a95e6e19
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-09-30 04:09:29 -0700

    Task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Prints the number of argumnets
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: return 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j, add = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		add += atoi(argv[i]);
+	}
+	printf("%d\n", add);
+	return (0);
+}
+
+ 
