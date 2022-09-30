4b0fe778c9a896ab14a36b39cfdbc200cda909e4
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-09-30 02:59:39 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Prints the name of the program
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: return 0
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
+
