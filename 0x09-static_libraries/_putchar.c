8c53c6cd1ca56a7a5c07f903ce46914e52ab0278
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-10-04 09:09:25 -0700

    Static libraries

---
+++ 0x09-static_libraries/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Prints _putchar
+ * putchar - Prints characters
+ * Return: always 0 (success)
+ */
+
+int _putchar(char c)
+{
+	/*
+	 * Prints _putchar
+	 */
+}
