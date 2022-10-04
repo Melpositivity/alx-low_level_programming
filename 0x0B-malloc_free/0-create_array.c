a6f8500e1ba54a5f019b735d10c100412a90d46e
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-10-04 02:58:47 -0700

    Malloc

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Prints the number of argumnets
+ * @size: argument count
+ * @c: argument vector
+ *
+ * Return: return 0
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *str;
+
+	str = malloc(sizeof(char) * size);
+	if (str == NULL || size == 0)
+	{
+		return (NULL);
+	}
+
+	i = 0;
+	while (i < size)
+	{
+		str[i] = c;
+		i++;
+	}
+	return (str);
+	free(str);
+}
+
+	
+	
