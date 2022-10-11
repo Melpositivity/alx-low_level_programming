e1bf8708fce43cee31e413da738d2f13f1b89d20
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-10-11 10:14:00 -0700

    0-object_like_macro.h

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,10 @@
+#ifndef _0_object_like_macro_h
+#define _0_object_like_macro_h
+
+/**
+ * MACRO
+ */
+
+#define SIZE 1024
+
+#endif
---
+++ 0x0D-preprocessor/main.h
@@ -0,0 +1,13 @@
+#ifndef _main_h
+#define _main_h
+
+/**
+ * Function prototypes
+ */
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+#endif
