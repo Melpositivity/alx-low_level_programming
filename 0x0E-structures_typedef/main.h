9ce4452778ab113d78c0513137020a95d59e2dc8
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-10-11 06:02:49 -0700

    dog.h

---
+++ 0x0E-structures_typedef/main.h
@@ -0,0 +1,9 @@
+#ifndef _main_h
+#define _main_h
+
+int _putchar(char c);
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+#endif
