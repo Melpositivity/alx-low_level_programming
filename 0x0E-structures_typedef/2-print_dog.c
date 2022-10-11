23815eb91e596c4e4b1569648ec8a594640bc567
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-10-11 08:32:06 -0700

    2 local objects

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -2,26 +2,24 @@
 #include <stdio.h>
 
 /**
- * print_dog - prints a struct dog.
- * @d: struct dog.
- * Return: no return.
+ * print_dog - prints a dog
+ *
+ * @d: dog to print
+ *
+ * Return: void
  */
-
 void print_dog(struct dog *d)
 {
-	if (d)
-	{
-		if (!(d->name))
-			printf("Name: (nil)\n");
-		else
-			printf("Name: %s\n", d->name);
+	if (d == 0)
+		return;
 
-		printf("Age: %f\n", d->age);
-
-		if (!(d->owner))
-
-			printf("Owner: (nil)\n");
-		else
-			printf("Owner: %s\n", d->owner);
-	}
-
+	if (d->name == 0)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	if (d->owner == 0)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+}
