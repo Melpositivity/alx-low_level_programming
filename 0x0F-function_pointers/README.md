3d987a23702e34d6c51a9572dd4950eda61d1feb
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-10-13 09:56:40 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,4 @@
+Write a function that prints a name.
+
+
+
---
+++ 0x0F-function_pointers/main.h
@@ -0,0 +1,11 @@
+#ifndef _main_h
+#define _main_h
+
+/**
+ * Function prototypes
+ */
+
+int _putchar(char c);
+void print_name(char *name, void (*f)
+
+#endif
