a642c2acee81ed503193176e9c727a4c5c2dd357
Emeldah Matengo <emeldahamatengo@gmail.com>
2022-10-31 05:35:55 -0700

    3-set_bit.c

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -10,13 +10,9 @@
  */
 int set_bit(unsigned long int *n, unsigned int index)
 {
-	unsigned int m;
-
 	if (index > 63)
 		return (-1);
 
-		m = 1 << index;
-		*n = (*n | m);
-
-		return (1);
+	*n = ((1UL << index) | *n);
+	return (1);
 }
