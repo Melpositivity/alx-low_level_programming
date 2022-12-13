aaea8493c8be19c73adf8c0a4469d6aaea8ed286
Melpositivity <emeldahamatengo@gmail.com>
2022-12-13 23:13:11 +0300

    Dynamic Libraries

---
+++ 0x18-dynamic_libraries/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string limited to n places
+ *
+ * @dest: string to concatenate
+ * @src: to contatenate string
+ * @n: byte limiter
+ * Return: array pointer dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int index;
+
+	for (index = 0; index < n && *(src + index) != '\0'; index++)
+	{
+		*(dest + index) = *(src + index);
+	}
+	while (index < n)
+	{
+		*(dest + index) = '\0';
+		index++;
+	}
+	return (dest);
+}
+
